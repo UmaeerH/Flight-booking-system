9e17a92355e896e82d538a0689701ce68dda597b
anisauddin03 <anisa.uddin03@gmail.com>
2025-01-06 01:18:15 +0000

    Made Flight JUnit Tests

--- FlightBookingSystem_Dist/src/bcu/cmp5332/bookingsystem/test/FlightTests.java
+++ FlightBookingSystem_Dist/src/bcu/cmp5332/bookingsystem/test/FlightTests.java
@@ -1,124 +1,132 @@
 package bcu.cmp5332.bookingsystem.test;
 
 import bcu.cmp5332.bookingsystem.model.*;
+import bcu.cmp5332.bookingsystem.model.Booking;
 import bcu.cmp5332.bookingsystem.main.FlightBookingSystemException;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class FlightTests {
 	private Flight testFlight;
 	private Customer testCustomer;
+	private Booking testBooking;
 	@Before
     public void setUp() {
         testFlight = new Flight(50, "AA123", "London", "New York", LocalDate.now(), 100, 500.0);
         testCustomer = new Customer(999, "Choi Yu-Jin", "42894829", "Choi@gmail.com");
+        testBooking = new Booking(1, testCustomer, testFlight, 500.0, LocalDate.now(), false);
     }
 
     @Test
     public void getIdTest() {
-        // TODO: Implement test for getId method
+    	assertEquals(50, testFlight.getId());
     }
 
-    @Test
+	@Test
     public void setIdTest() {
-        // TODO: Implement test for setId method
+		testFlight.setId(23);
+		assertEquals(23, testFlight.getId());
     }
 
     @Test
     public void getFlightNumberTest() {
-        // TODO: Implement test for getFlightNumber method
+    	assertEquals("AA123", testFlight.getFlightNumber());
     }
 
     @Test
     public void setFlightNumberTest() {
-        // TODO: Implement test for setFlightNumber method
+    	testFlight.setFlightNumber("LS3421");
+    	assertEquals("LS3421", testFlight.getFlightNumber());
     }
 
     @Test
     public void getOriginTest() {
-        // TODO: Implement test for getOrigin method
+    	assertEquals("London", testFlight.getOrigin());
     }
 
     @Test
     public void setOriginTest() {
-        // TODO: Implement test for setOrigin method
+    	testFlight.setOrigin("Birmingham");
+    	assertEquals("Birmingham", testFlight.getOrigin());
     }
 
     @Test
     public void getDestinationTest() {
-        // TODO: Implement test for getDestination method
+    	assertEquals("New York", testFlight.getDestination());
     }
 
     @Test
     public void setDestinationTest() {
-        // TODO: Implement test for setDestination method
+    	testFlight.setDestination("Bucharest Otopeni");
+    	assertEquals("Bucharest Otopeni", testFlight.getDestination());
     }
 
     @Test
     public void getDepartureDateTest() {
-        // TODO: Implement test for getDepartureDate method
+    	assertEquals(LocalDate.now(), testFlight.getDepartureDate());
     }
 
     @Test
     public void setDepartureDateTest() {
-        // TODO: Implement test for setDepartureDate method
+    	testFlight.setDepartureDate(LocalDate.of(2025, 9, 13));
+    	assertEquals(LocalDate.of(2025, 9, 13), testFlight.getDepartureDate());
     }
 
     @Test
     public void getCapacityTest() {
-        // TODO: Implement test for getCapacity method
+    	assertEquals(100, testFlight.getCapacity());
     }
 
     @Test
     public void setCapacityTest() {
-        // TODO: Implement test for setCapacity method
+    	testFlight.setCapacity(78);
+    	assertEquals(78, testFlight.getCapacity());
     }
 
     @Test
     public void getPriceTest() {
-        // TODO: Implement test for getPrice method
+    	assertEquals(500.0, testFlight.getPrice());
     }
 
     @Test
     public void setPriceTest() {
-        // TODO: Implement test for setPrice method
+    	testFlight.setPrice(749.91);
+    	assertEquals(749.91, testFlight.getPrice());
     }
 
     @Test
     public void getFreeCapacityTest() {
-        // TODO: Implement test for getFreeCapacity method
-    }
-
-    @Test
-    public void setFreeCapacityTest() {
-        // TODO: Implement test for setFreeCapacity method
+    	assertEquals(100, testFlight.getFreeCapacity());
     }
 
     @Test
     public void getPassengersTest() {
-        // TODO: Implement test for getPassengers method
+    	assertEquals(Collections.emptyList(), testFlight.getPassengers());
     }
 
     @Test
     public void addPassengerTest() {
-        // TODO: Implement test for addPassenger method
+    	List<Customer> customerList = new ArrayList<>();
+    	customerList.add(testCustomer);
+    	testFlight.addPassenger(testCustomer);
+    	assertEquals(customerList, testFlight.getPassengers());
     }
 
     @Test(expected = FlightBookingSystemException.class)
     public void removePassengerTest() throws FlightBookingSystemException {
-        // TODO: Implement test for removePassenger method
-        // This test should ensure that an exception is thrown when trying to remove a passenger who is not on the flight
+    	testFlight.removePassenger(testCustomer);
     }
 
     @Test
     public void getDetailsShortTest() {
-        // TODO: Implement test for getDetailsShort method
-    }
-
-    @Test
-    public void getDetailsLongTest() {
-        // TODO: Implement test for getDetailsLong method
+    	assertEquals("Flight #50 - AA123 - London to New York on " + LocalDate.now() + "\nSeats filled: 0/100", testFlight.getDetailsShort());
     }
 }
